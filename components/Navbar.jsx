53305f4080e03a29325d7213f466e0748da4a11a
Ruth Aja <ruthajalah1@gmail.com>
2022-11-29 13:43:12 +0700

    disable eslinter

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,13 +1,12 @@
-'use client';
+'use client'
 
-import { motion } from 'framer-motion';
+import { motion } from 'framer-motion'
 import styles from '../styles'
-import {navVariants} from '../utils/motion'
+import { navVariants } from '../utils/motion'
 
 const Navbar = () => (
-  <motion.nav>
-    navbar
-  </motion.nav>
-);
-
-export default Navbar;
+    <motion.nav variants={navVariants} initial='hidden' whileInView='show' className={`${styles.xPaddings} py-8 relative`}>
+        navbar
+    </motion.nav>
+)
+export default Navbar
