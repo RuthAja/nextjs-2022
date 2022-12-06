418096a1d7b8aef22b1468d0fe6bce91fabd3611
Ruth Aja <ruthajalah1@gmail.com>
2022-12-06 11:18:37 +0700

    modified CostumText and add text Explore

--- sections/Explore.jsx
+++ sections/Explore.jsx
@@ -1,9 +1,40 @@
-'use client';
+'use client'
 
-const Explore = () => (
-  <section>
-    Explore section
-  </section>
-);
+import { useState } from 'react'
+import { motion } from 'framer-motion'
+import styles from '../styles'
+import { staggerContainer } from '../utils/motion'
+import { TypingText, ExploreCard, TitleText } from '../components'
+import { exploreWorlds } from '../constants'
 
-export default Explore;
+const Explore = () => {
+    const [active, setActive] = useState('world-2')
+    return (
+        <section className={`${styles.paddings}`} id='explore'>
+            <motion.div
+                variants={staggerContainer}
+                initial='hidden'
+                whileInView='show'
+                viewport={{ once: false, amount: 0.25 }}
+                className={`${styles.innerWidth} mx-auto flex flex-col`}>
+                <TypingText tittle='| The World' textStyles={'text-center'} />
+                <TitleText
+                    title={
+                        <>
+                            Pilih dunia yang kamu inginkan <br className='hidden md:block' />
+                            Untuk Jelajahi
+                        </>
+                    }
+                    textStyles='text-center'
+                />
+                <div className={'mt-[50px] flex lg:flex-row flex-col min-h-[70vh] gap-5'}>
+                    {exploreWorlds.map((world, index) => (
+                        <ExploreCard key={world.id} {...world} index={index} active={active} hadleClick={setActive} />
+                    ))}
+                </div>
+            </motion.div>
+        </section>
+    )
+}
+
+export default Explore
