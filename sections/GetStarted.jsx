0aa022cee71b28361147b763edf87d0e2fad2450
Ruth Aja <ruthajalah1@gmail.com>
2022-12-07 11:27:34 +0700

    edit NewFeatires amd WhatsNew section

--- sections/GetStarted.jsx
+++ sections/GetStarted.jsx
@@ -17,7 +17,7 @@ const GetStarted = () => (
             <motion.div variants={planetVariants('left')} className={`flex-1 ${styles.flexCenter}`}>
                 <img src='./get-started.png' alt='get started' className={'w-[90%] h-[90%] object-contain'} />
             </motion.div>
-            <motion.div variants={fadeIn('left', 'tween', 0.2, 1)} className={'flex-[0.75] flex justify-center flex-col'}>
+            <motion.div variants={fadeIn('left', 'tween', 0.2, 1)} className={'flex-[0.95] flex justify-center flex-col'}>
                 <TypingText title={'| How Intelligent Works'} />
                 <TitleText title={<>Di mulai dengan beberapa klik</>} />
                 <div className={'mt-[31px] flex flex-col max-w-[320px] gap-[24px] '}>
