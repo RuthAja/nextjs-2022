a76546cafeb0025e9a7ab2433b4591f32e7c4c58
Ruth Aja <ruthajalah1@gmail.com>
2022-12-07 14:18:42 +0700

    edit Wolrd section

--- sections/World.jsx
+++ sections/World.jsx
@@ -18,24 +18,17 @@ const World = () => (
                 title={<>Lacak teman-teman di sekitar Anda dan undang mereka untuk bermain bersama di dunia yang sama</>}
                 textStyles={'text-center'}
             />
-      <motion.div variants={fadeIn('up', 'tween', 0.3, 1)} className={'relative mt-[68px] flex w-full h-[550px]'}>
-        <img
-        src='./map.png'
-        alt='map'
-        className={'w-full h-full object-cover'}
-        />
-        <div className={'absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'}>
-          <img
-          src='./people-01.png' alt='orang' className={'w-full h-full'} />
-        </div>
-        <div className={'absolute top-20 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'}>
-          <img
-          src='./people-02.png' alt='orang 2' className={'w-full h-full'} />
-        </div>
-        <div className={'absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'}>
-          <img
-          src='./people-03.png' alt='orang' className={'w-full h-full'} />
-        </div>
+            <motion.div variants={fadeIn('up', 'tween', 0.3, 1)} className={'relative mt-[68px] flex w-full h-[550px]'}>
+                <img src='./map.png' alt='map' className={'w-full h-full object-cover'} />
+                <div className={'absolute bottom-20 right-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'}>
+                    <img src='./people-01.png' alt='orang' className={'w-full h-full'} />
+                </div>
+                <div className={'absolute top-20 left-20 w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'}>
+                    <img src='./people-02.png' alt='orang 2' className={'w-full h-full'} />
+                </div>
+                <div className={'absolute top-1/2 left-[45%] w-[70px] h-[70px] p-[6px] rounded-full bg-[#5d6680]'}>
+                    <img src='./people-03.png' alt='orang' className={'w-full h-full'} />
+                </div>
             </motion.div>
         </motion.div>
     </section>
